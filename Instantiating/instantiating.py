6ddbad0fdbbc8a0f65525af223f34ace3a414d8f
saradiaz-UPC-BCN <sara.diaz@upc.edu>
2019-02-27 18:55:32 +0100

    Added data for 2015, 2016, 2017, 2018

--- Instantiating/instantiating.py
+++ Instantiating/instantiating.py
@@ -1,16 +1,14 @@
 import json
 
-from neo4j import GraphDatabase
+#from neo4j import GraphDatabase
 
-uri = 'bolt://localhost:7687'
-driver = GraphDatabase.driver(uri, auth=('', ''))
-
+#uri = 'bolt://localhost:7687'
+#driver = GraphDatabase.driver(uri, auth=('', ''))
 
 journal_papers_string = open('./journal_papers.json', 'r').read()
 
 # Exctracting the list of all journal papers
 journal_papers_json = json.loads(journal_papers_string)['result']['hits']['hit']
-
 
 # Creating data structures
 
@@ -23,9 +21,10 @@ articles = set()
 # Creating relations
 
 writes = []
+belongs_to = []
+has_a = set()
 
-
-for jp in journal_papers_json :
+for jp in journal_papers_json:
     journal_paper = jp['info']
     journal_authors = journal_paper['authors']
     article_title = journal_paper['title']
@@ -33,30 +32,46 @@ for jp in journal_papers_json :
     volume = journal_paper['volume']
     year = journal_paper['year']
 
-    if isinstance(journal_authors['author'], list) :
-        for author in journal_authors['author'] :
+    if isinstance(journal_authors['author'], list):
+        for author in journal_authors['author']:
             authors.add(author + '\n')
             writes.append(author + ',' + article_title + '\n')
-    else :
+    else:
         authors.add(journal_authors['author'])
 
     articles.add(article_title + '\n')
+    journals.add(journal + '\n')
+    belongs_to.append(article_title + ',' + journal + '\n')
+    volumes.add(volume + '\n')
+    has_a.add(journal + ',' + volume + '\n')
 
 
-with open('./authors.csv', 'w') as f:
-    f.writelines(authors)
 
-with open('./articles.csv', 'w') as f:
-    f.writelines(articles)
+#with open('./articles.csv', 'w') as f:
+    #f.writelines(articles)
 
-with open('./writes.csv', 'w') as f:
-    f.writelines(writes)
+# with open('./authors.csv', 'w') as f:
+    # f.writelines(authors)
 
-def import_authors(tx, authors_csv) :
-    tx.run("load csv from {authors_csv} as line "
-           "create (:Author {name:line[0]})", authors_csv=authors_csv)
+#with open('./writes.csv', 'w') as f:
+    #f.writelines(writes)
 
-with driver.session() as session :
-    session.read_transaction(import_authors, 'authors.csv')
+with open('./journals.csv', 'w') as f:
+    f.writelines(journals)
+
+#with open('./belongsTo.csv', 'w') as f:
+    #f.writelines(belongs_to)
+
+#with open('./volumes.csv', 'w') as f:
+    #f.writelines(volumes)
+
+#with open('./hasA.csv', 'w') as f:
+    #f.writelines(has_a)
+
+#def import_authors(tx, journals_csv):
+   #tx.run("load csv from {authors_csv} as line "
+           #"create (:Author {name:line[0]})", authors_csv=journals_csv)
 
 
+#with driver.session() as session:
+   # session.read_transaction(import_authors, 'authors.csv')
--- Instantiating/uploadCSV.py
+++ Instantiating/uploadCSV.py
@@ -1,10 +1,9 @@
 from neo4j import GraphDatabase
 
-
 uri = 'bolt://localhost:7687'
 driver = GraphDatabase.driver(uri, auth=('', ''))
 
-
+#Authors
 def create_an_author(tx):
     result = tx.run("LOAD CSV FROM "
                     "'https://raw.githubusercontent.com/hugrave-upc/SDM_Lab1/master/Instantiating/authors.csv' "
@@ -12,7 +11,7 @@ def create_an_author(tx):
                     "CREATE (:Author { name: line[0]})")
     print(result)
 
-
+#Articles
 def create_an_article(tx):
     result = tx.run("LOAD CSV FROM "
                     "'https://raw.githubusercontent.com/hugrave-upc/SDM_Lab1/master/Instantiating/articles.csv' "
@@ -20,7 +19,7 @@ def create_an_article(tx):
                     "CREATE (:Article { title: line[0]})")
     print(result)
 
-
+#Writes
 def create_writes(tx):
     result = tx.run("LOAD CSV FROM "
                     "'https://raw.githubusercontent.com/hugrave-upc/SDM_Lab1/master/Instantiating/writes.csv' "
@@ -32,6 +31,53 @@ def create_writes(tx):
                     "return r")
     print(result)
 
+#Journals
+def create_a_journal(tx):
+    result = tx.run("LOAD CSV FROM "
+                    "'https://raw.githubusercontent.com/sdiazben/git_local_repository/master/journals.csv' "
+                    "AS line FIELDTERMINATOR ','"
+                    "CREATE (:Journal { name: line[0]})")
+    print(result)
+
+
+#Belongs to
+def belongs_to(tx):
+    result = tx.run("LOAD CSV FROM "
+                    "'https://raw.githubusercontent.com/sdiazben/git_local_repository/master/belongsTo.csv'"
+                    "AS line FIELDTERMINATOR ','"
+                    "match (auth:Article), (art:Journal) "
+                    "where auth.title = line[0] "
+                    "and art.name = line[1]"
+                    "create (auth)-[r:belongsTo]->(art) "
+                    "return r")
+    print(result)
+
+#Volume
+def create_a_volume(tx):
+    result = tx.run("LOAD CSV FROM "
+                    "'https://raw.githubusercontent.com/sdiazben/git_local_repository/master/volumes.csv' "
+                    "AS line FIELDTERMINATOR ','"
+                    "CREATE (:Volume { number: line[0]})")
+    print(result)
+
+
+#hasA
+def create_has_a(tx):
+    result = tx.run("LOAD CSV FROM "
+                    "'https://raw.githubusercontent.com/sdiazben/git_local_repository/master/hasA.csv'"
+                    "AS line FIELDTERMINATOR ','"
+                    "match (auth:Journal), (art:Volume) "
+                    "where auth.name = line[0] "
+                    "and art.number = line[1]"
+                    "create (auth)-[r:hasA]->(art) "
+                    "return r")
+    print(result)
 
 with driver.session() as session:
-    session.read_transaction(create_writes)
+    #session.read_transaction(create_an_author)
+    #session.read_transaction(create_an_article)
+    #session.read_transaction(create_writes)
+    #session.read_transaction(create_a_journal)
+    #session.read_transaction(belongs_to)
+    #session.read_transaction(create_a_volume)
+    session.read_transaction(create_has_a)
