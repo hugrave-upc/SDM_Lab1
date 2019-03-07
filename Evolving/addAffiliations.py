dbf1252cb5159527c282de0503b587773380851b
hugrave <edoardo.conte@live.it>
2019-03-07 19:59:29 +0100

    Added partial affiliation support

---
+++ Evolving/addAffiliations.py
@@ -0,0 +1,58 @@
+# Extending the graph with the reviews
+
+from neo4j import GraphDatabase
+
+import random, sys
+
+uri = 'bolt://localhost:7687'
+driver = GraphDatabase.driver(uri, auth=('', ''))
+
+sep_value = '\t'
+
+# Extracting all the authors
+def get_authors(tx):
+    authors = tx.run("match (a:Author) return a.name as name;")
+    return authors
+
+# Extracting all the companies
+def get_companies(tx):
+    companies = tx.run("match (c:Company) return c.name as name;")
+    return companies
+
+# Extracting all the universities
+def get_universities(tx):
+    universities = tx.run("match (u:University) return u.name as name;")
+    return universities
+
+# Setting the affiliation with university
+def university_affiliated(tx, authorName, uniName):
+    tx.run("match (auth:Author {name: {authorName}}) "
+           "create (auth)-[:Affiliated_to]->(:University {name: {uniName}});",
+           authorName=authorName, uniName=uniName)
+
+# Setting the affiliation with company
+def company_affiliated(tx, authorName, compName):
+    tx.run("match (auth:Author {name: {authorName}}) "
+           "create (auth)-[:Affiliated_to]->(:Company {name: {compName}});",
+           authorName=authorName, compName=compName)
+
+
+
+with driver.session() as session:
+    authors = session.read_transaction(get_authors)
+    companies = session.read_transaction(get_companies)
+    universities = session.read_transaction(get_universities)
+    comp_list = list(companies)
+    uni_list = list(universities)
+
+affiliations = set()
+for author in authors:
+    isCompany = bool(random.getrandbits(1))
+    if isCompany:
+        affiliations.add(sep_value.join([author['name'], random.choice(comp_list)['name'], 'Company\n']))
+    else:
+        affiliations.add(sep_value.join([author['name'], random.choice(uni_list)['name'], 'University\n']))
+
+with open('./out_csv/affiliations.csv', 'w') as f:
+    f.write(sep_value.join(['author', 'institution name', 'Company|University\n']))
+    f.writelines(affiliations)
